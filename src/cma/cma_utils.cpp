be61a9b2dc6a3c1245e8fc664fe253e6b34a7437
GoldJohnKing <GoldJohnKing@Live.cn>
2021-12-25 01:38:13 +0800

    Use a much simpler way to determine reserved huge pages count

--- src/cma/cma_utils.cpp
+++ src/cma/cma_utils.cpp
@@ -5,27 +5,22 @@
 
 int CmaGetReservedHugePagesCount(void)
 {
-	int reservedHugePagesCount = 0;
-
 	MEMORYSTATUSEX mem_status;
 	mem_status.dwLength = sizeof(mem_status);
+
 	GlobalMemoryStatusEx(&mem_status);
 
-	if (mem_status.ullAvailPhys > 4294967296) // Avail > 4G
-		reservedHugePagesCount = 1;
+	if (mem_status.ullTotalPhys > 12884901888) // Avail > 12G
+		return 6;
 
 	if (mem_status.ullAvailPhys > 8589934592) // Avail > 8G
-	{
-		reservedHugePagesCount = 2;
+		return 4;
 
-		if (mem_status.ullTotalPhys > 17179869184) // Avail > 8G && Total > 16G
-		{
-			reservedHugePagesCount = 4;
+	if (mem_status.ullAvailPhys > 6442450944) // Avail > 6G
+		return 2;
 
-			if (mem_status.ullAvailPhys > 12884901888) // Total > 16G && Avail > 12G
-				reservedHugePagesCount = 6;
-		}
-	}
-
-	return reservedHugePagesCount;
+	if (mem_status.ullAvailPhys > 4294967296) // Avail > 4G
+		return 1;
+	
+	return 0;
 }
