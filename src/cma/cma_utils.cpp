e588e0e75772c203e017b45dc7ae3b8b3f32681a
sfc9982 <26681591+sfc9982@users.noreply.github.com>
2021-12-29 15:01:04 +0800

    Dynamically calculate the size of HugePage

--- src/cma/cma_utils.cpp
+++ src/cma/cma_utils.cpp
@@ -5,26 +5,25 @@
 
 int CmaGetReservedHugePagesCount(void)
 {
+
 #ifdef WITH_LOCK_PAGES
+
 	MEMORYSTATUSEX mem_status;
 	mem_status.dwLength = sizeof(mem_status);
 
 	GlobalMemoryStatusEx(&mem_status);
 
-	if (mem_status.ullAvailPhys > 17179869184) // Avail > 16G
-		return 8;
-
-	if (mem_status.ullAvailPhys > 12884901888) // Avail > 12G
-		return 6;
+	const uint64_t GSIZE = 1024 * 1024 * 1024; // "ull" in "ullAvailPhys" indicates it is unsigned long long type, so use "uint64_t" here
 
 	if (mem_status.ullAvailPhys > 8589934592) // Avail > 8G
-		return 4;
+		return mem_status.ullAvailPhys / GSIZE / 2; // Reserved size is set to half of the avail phys_mem
 
 	if (mem_status.ullAvailPhys > 6442450944) // Avail > 6G
 		return 2;
 
 	if (mem_status.ullAvailPhys > 4294967296) // Avail > 4G
 		return 1;
+
 #endif // WITH_LOCK_PAGES
 
 	return 0;
