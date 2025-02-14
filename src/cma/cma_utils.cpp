c5bd63e89bf8cb1a2e33a092f0e75b867e9015a1
Gold John King <GoldJohnKing@Live.cn>
2025-02-14 11:00:10 +0800

    Prioritize user defined options in environment variables

--- src/cma/cma_utils.cpp
+++ src/cma/cma_utils.cpp
@@ -4,11 +4,13 @@
 
 void CmaSetMemoryAllocatorRuntimeOptions(void)
 {
-	mi_option_set(mi_option_arena_eager_commit, 1); // Always eager commit arenas, which has a significant performance improvement on Arma 3
+    // Use `mi_option_set_default` instead of `mi_option_set` to prioritize user defined options in environment variables
 
-	mi_option_set(mi_option_allow_large_os_pages, 1); // Always use large pages when available
+	mi_option_set_default(mi_option_arena_eager_commit, 1); // Always eager commit arenas, which has a significant performance improvement on Arma 3
 
-    mi_option_set(mi_option_eager_commit_delay, 0); // Always eager commit segments
+	mi_option_set_default(mi_option_allow_large_os_pages, 1); // Always use large pages when available
 
-	mi_option_set(mi_option_purge_delay, -1); // Do not purge memory back to the OS
+	mi_option_set_default(mi_option_eager_commit_delay, 0); // Always eager commit segments
+
+	mi_option_set_default(mi_option_purge_delay, -1); // Do not purge memory back to the OS
 }
